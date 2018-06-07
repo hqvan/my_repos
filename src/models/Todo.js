3beb9f33cbb5f2161a528bf056f1f9f109ed3e6f
Van Ha <hqvan206@gmail.com>
2018-06-06 22:18:45 -0700

    final version

---
+++ src/models/Todo.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+class TodoModel {
+  static all(){
+    let request = axios.get("https://super-crud.herokuapp.com/api/todos")
+    return request
+  }
+
+  static create(todo) {
+  	let request = axios.post("https://super-crud.herokuapp.com/api/todos", todo)
+  	return request
+  }
+  static delete(todo){
+  let request = axios.delete(`https://super-crud.herokuapp.com/api/todos/${todo._id}`)
+  return request
+}
+}
+
+export default TodoModel
+
